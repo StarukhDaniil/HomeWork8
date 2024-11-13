b9187e197ebe2ba1f368b8b6d0c5a15a5d3140c5
StarukhDaniil <starukh.danil@gmail.com>
2024-11-13 22:43:57 +0200

    Added basic aspects of sharedPointer

--- sharedPointer.h
+++ sharedPointer.h
@@ -1,11 +1,47 @@
 #pragma once
 #include <iostream>
 
+
 template <typename T>
 class sharedPointer {
 private:
 	int* counter = new int(0);
 	T* ptr = nullptr;
 public:
+	sharedPointer(T* ptr) {
+		++(*counter);
+		this->ptr = ptr;
+	}
 	
+	sharedPointer(const sharedPointer<T>& other) {
+		this->counter = other.counter;
+		this->ptr = other.ptr;
+		++(*counter);
+	}
+
+	sharedPointer<T>& operator=(const sharedPointer<T>& other) {
+		this->counter = other.counter;
+		this->ptr = other.ptr;
+		++(*counter);
+	}
+
+	T& operator*() const {
+		return *ptr;
+	}
+
+	T* operator->() const {
+		return ptr;
+	}
+
+	operator bool() const {
+		return (ptr != nullptr) || (ptr != NULL);
+	}
+
+	~sharedPointer() noexcept {
+		--(*counter);
+		if (*counter == 0) {
+			delete counter;
+			delete ptr;
+		}
+	}
 };
